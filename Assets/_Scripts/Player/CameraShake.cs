0d79dc2f528ad26eef59f34e6a8822f135825b8e
Grgmaistor <grga.galicpuskar@elev.ga.lbs.se>
2022-10-19 21:57:08 +0200

    Flash and shake
    Now the camera has a flash and can shake.

--- Assets/_Scripts/Player/CameraShake.cs
+++ Assets/_Scripts/Player/CameraShake.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
-    [SerializeField] float intensity = 0;
     [SerializeField] Camera camera;
+    [Header("Shake")]
+    [SerializeField] float intensity = 0;
     [SerializeField] bool shake;
+    [Header("Flash")]
+    [SerializeField] float fadeDuration = 0;
+    [SerializeField] float flashDuration = 0;
+    [Tooltip("Add a image into the local canvas that is set to screen size. Add the image source as \"Assets/Textures/JUICE/Flash.png\" and put its alpha value to zero.")]
+    [SerializeField] UnityEngine.UI.Image flashImage;
+    [SerializeField] bool flash;
 
     Vector3 _StartPos;
+    Coroutine _CurrentFadeRoutine;
 
     // Start is called before the first frame update
     void Start()
     {
-        _StartPos = camera.transform.position;
+        _StartPos = camera.transform.position; // Start position of camera
+        flashImage.color = new Color(flashImage.color.r, flashImage.color.g, flashImage.color.b, 0); // Hide flash
     }
 
     // Update is called once per frame
@@ -21,13 +30,36 @@ public class CameraShake : MonoBehaviour
     {
         if (shake)
         {
-            Vector3 pos = Random.insideUnitCircle * intensity;
-            pos.z = _StartPos.z;
-            camera.transform.position = pos;
+            Vector3 pos = Random.insideUnitCircle * intensity; // Shake
+            pos.z = _StartPos.z; // Remove z axis value
+            camera.transform.position = pos; // Apply to camera
         }
-        else if(camera.transform.position != _StartPos)
+        else if(camera.transform.position != _StartPos) // If position is not start position
         {
-            camera.transform.position = _StartPos;
+            camera.transform.position = _StartPos; // Reset position
         }
+        if (flash || Input.GetKeyDown(KeyCode.S)) // Flash
+        {
+            flash = false;
+            if (_CurrentFadeRoutine != null)
+            {
+                StopCoroutine(_CurrentFadeRoutine); // Stop current flash
+            }
+           _CurrentFadeRoutine = StartCoroutine(Fade()); // Start flash
+        }
+    }
+    IEnumerator Fade()
+    {
+        flashImage.color = new Color(flashImage.color.r, flashImage.color.g, flashImage.color.b, 1); // Show white image
+        float timer = fadeDuration; // Keep on for some time
+        yield return new WaitForSeconds(flashDuration);
+        while (timer > 0)
+        {
+            timer -= Time.deltaTime; 
+            flashImage.color = new Color(flashImage.color.r, flashImage.color.g, flashImage.color.b, timer/fadeDuration); // Apply fade
+            yield return null;
+        }
+
+        flashImage.color = new Color(flashImage.color.r, flashImage.color.g, flashImage.color.b, 0); // Set alpha to zero after done to be sure
     }
 }
